87dbd51971d9bc0f9a093cb3e0e7588952020855
Nazgul28 <lfmassola@gmail.com>
2016-01-29 20:04:42 -0800

    styling the theme - fontawesome and removing left menu

---
+++ wp-content/themes/spiritism_van/functions.php
@@ -0,0 +1,12 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+}
+function enqueue_our_required_stylesheets(){
+	wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'); 
+}
+add_action('wp_enqueue_scripts','enqueue_our_required_stylesheets');
+
+?>
